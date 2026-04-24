39505d391b4ffa2bd85d68535cf68e024c056fb2
David Adekoya <davidadekoya88@gmail.com>
2026-04-24 18:29:20 +0100

    done with proejcte scteup

---
+++ fyeo/lib/models/encrypted_file.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'encrypted_file.freezed.dart';
+part 'encrypted_file.g.dart';
+
+@freezed
+class EncryptedFile with _$EncryptedFile {
+  const factory EncryptedFile({
+    required String id,
+    required String fileName,
+    required String filePath,
+    required int originalSize,
+    required int encryptedSize,
+    required DateTime createdAt,
+    required DateTime lastModified,
+    required String compressionRatio, // e.g., "-4.0%"
+    required bool isSmartCompressed,
+  }) = _EncryptedFile;
+
+  factory EncryptedFile.fromJson(Map<String, dynamic> json) =>
+      _$EncryptedFileFromJson(json);
+}
